31516f1d989298ddc0a699b1473e569dfcf67eb3
knp84 <sabanovroman84@gmail.com>
2025-03-29 15:26:15 +0500

    1.0

--- app/handlers.py
+++ app/handlers.py
@@ -18,9 +18,12 @@ router = Router()
 async def send_welcome(message: Message):
    await message.answer("Начало работы \nВышлите мне изображение ваших оценок")
 
+
 @router.message(F.photo)
 async def photo_message(message: Message, bot: Bot):
     global avr_score
+    global sum_score
+    global count_score
     if message.photo:
         file_name = f"photos/{message.photo[-1].file_id}.jpg"
         await bot.download(message.photo[-1], destination=file_name)
@@ -31,43 +34,41 @@ async def photo_message(message: Message, bot: Bot):
         sum_score = 0
         count_score = 0
 
-        for i in range(len(s) - 1):
+        for i in range(len(s.strip('\n'))):
             sum_score += int(s[i])
             count_score += 1
 
         avr_score = sum_score / count_score
-        print(avr_score)
+    await message.reply(f'ваш средний балл {round(avr_score, 3)}\nКакую оценку вы хотите за четверть?(Введите цифру)')
+    
+    
+@router.message(F.text)
+async def having_grade(message: Message):
 
-        question = input('До какого числа следует посчитать?(Введите цифру)\n')
+    if message.text == '4':
+        if 4.5 > avr_score >= 3.5:
+            await message.answer('У вас уже 4 или 5')
+        else:
+            count_score_5 = count_score_4 = avr_score_5 = avr_score_4 = 0
+            sum_score_5 = sum_score_4 = sum_score
+            while avr_score_5 < 3.5:
+                sum_score_5 += 5
+                count_score_5 += 1
+                avr_score_5 = sum_score_5 / (count_score_5 + count_score)
+            while avr_score_4 < 3.5:
+                sum_score_4 += 4
+                count_score_4 += 1
+                avr_score_4 = sum_score_4 / (count_score_4 + count_score)
+            await message.answer(f'До {round(avr_score_4, 3)} вам нужно {count_score_5} "5" или {count_score_4} "4"')
+    elif message.text == '5':
+        if avr_score >= 4.5:
+            await message.answer('У вас уже 5')
+        else:
+            sum_score_5_2 = sum_score
+            count_score_5_2 = avr_score_5_2 = 0
+            while avr_score_5_2 < 4.5:
+                sum_score_5_2 += 5
+                count_score_5_2 += 1
+                avr_score_5_2 = sum_score_5_2 / (count_score_5_2 + count_score)
+            await message.answer(f'До {round(avr_score_5_2, 3)} вам осталось {count_score_5_2} "5"')
 
-        if question == '4':
-            if 4.5 > avr_score >= 3.5:
-                print('У вас уже 4 или 5')
-            else:
-                count_score_5 = count_score_4 = avr_score_5 = avr_score_4 = 0
-                sum_score_5 = sum_score_4 = sum_score
-                while avr_score_5 < 3.5:
-                    sum_score_5 += 5
-                    count_score_5 += 1
-                    avr_score_5 = sum_score_5 / (count_score_5 + count_score)
-                while avr_score_4 < 3.5:
-                    sum_score_4 += 4
-                    count_score_4 += 1
-                    avr_score_4 = sum_score_4 / (count_score_4 + count_score)
-                print(f'До {avr_score_4} вам нужно {count_score_5} пятёрок или {count_score_4} четвёрок')
-        elif question == '5':
-            if avr_score >= 4.5:
-                print('У вас уже 5')
-            else:
-                sum_score_5_2 = sum_score
-                count_score_5_2 = avr_score_5_2 = 0
-                while avr_score_5_2 < 4.5:
-                    sum_score_5_2 += 5
-                    count_score_5_2 += 1
-                    avr_score_5_2 = sum_score_5_2 / (count_score_5_2 + count_score)
-                print(f'До {avr_score_5_2} вам осталось {count_score_5_2} пятерок')
-
-'''@router.message(F.text=='выбор оценки')
-async def grades_quest(message: Message):
-   await message.answer('')
-'''
