a166ecd683a7abf67fd09cb3cebfa624cbd898cb
knp84 <sabanovroman84@gmail.com>
2025-09-22 23:57:53 +0500

    0

--- app/db_api/schemas/user.py
+++ app/db_api/schemas/user.py
@@ -1,3 +1,11 @@
 from app.db_api.db_gino import TimedBaseModel
 
-Class User(T)
+from sqlalchemy import Column, BigInteger, String, sql
+
+class User(TimedBaseModel):
+    __tablename__ = 'users'
+    user_id = Column(BigInteger, primary_key=True)
+    name = Column(String(20), primary_key=True)
+    update_name = Column(String(30), primary_key=True)
+
+    #query: sql.select
